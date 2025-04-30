1d22e6e5b06b8fa391b4355082eaf45d019a7e4e
Christopher-Cannon <Christopher-Cannon@users.noreply.github.com>
2025-04-30 16:39:57 +0100

    Implement image gallery component

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,5 +1,27 @@
 <script setup>
+import { ref } from "vue";
 import PageSection from "./PageSection.vue";
+
+const data = [
+    {
+        src: "/home-gallery/example-1.jpg",
+        alt: "Restored blue guitar",
+    },
+    {
+        src: "/home-gallery/example-2.jpg",
+        alt: "Modified les paul guitar",
+    },
+    {
+        src: "/home-gallery/example-3.jpg",
+        alt: "Restored acoustic guitar",
+    },
+    {
+        src: "/home-gallery/example-4.jpg",
+        alt: "Repaired acoustic guitar",
+    },
+];
+
+const activeIndex = ref(0);
 </script>
 
 <template>
@@ -8,5 +30,51 @@ import PageSection from "./PageSection.vue";
         bgColour="bg-stone-900"
         fgColour="text-white"
     >
+        <div class="relative h-[300px] md:h-[500px] lg:h-[700px] mt-8">
+            <img
+                v-for="(item, index) in data"
+                :key="index"
+                :src="item.src"
+                :alt="item.alt"
+                class="absolute z-1 display-block object-cover h-[300px] md:h-[500px] lg:h-[700px] w-full"
+                :class="{ active: activeIndex === index }"
+            />
+
+            <button
+                class="absolute cursor-pointer bg-stone-800 z-10 top-[calc(50%-23px)] lg:top-[calc(50%-32px)] ml-1 lg:ml-2 rounded-full w-[46px] h-[46px] lg:w-[64px] lg:h-[64px]"
+                @click="
+                    activeIndex =
+                        activeIndex < 0 ? data.length - 1 : activeIndex - 1
+                "
+            >
+                <font-awesome-icon
+                    :icon="['fas', 'angle-left']"
+                    class="text-3xl lg:text-4xl mt-0.75"
+                />
+            </button>
+
+            <button
+                class="absolute cursor-pointer bg-stone-800 z-10 top-[calc(50%-23px)] lg:top-[calc(50%-32px)] right-0 mr-1 lg:mr-2 rounded-full w-[46px] h-[46px] lg:w-[64px] lg:h-[64px]"
+                @click="
+                    activeIndex =
+                        activeIndex === data.length ? 0 : activeIndex + 1
+                "
+            >
+                <font-awesome-icon
+                    :icon="['fas', 'angle-right']"
+                    class="text-3xl lg:text-4xl mt-0.75"
+                />
+            </button>
+        </div>
     </PageSection>
 </template>
+
+<style scoped>
+img {
+    display: none;
+}
+
+.active {
+    display: block;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faBars);
 library.add(faXmark);
+library.add(faAngleLeft);
+library.add(faAngleRight);
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
