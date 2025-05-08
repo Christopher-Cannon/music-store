2a431f2c5b4c9deb9b7dc01f5c40c04a4a3feaa7
Chris Cannon <Christopher-Cannon@users.noreply.github.com>
2025-05-08 11:37:36 +0100

    Portfolio page nominally complete

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -7,12 +7,17 @@ import projects from "../data/projects";
     <Wrapper>
         <h2 class="text-2xl md:text-3xl lg:text-4xl text-center font-black my-10">Portfolio</h2>
 
-        <div v-for="project in projects" :key="project.id" class="my-10">
-            <h3 class="font-black text-lg md:text-xl lg:text-2xl mt-5 lg:mt-10 mb-5">{{ project.title }}</h3>
+        <div v-for="project in projects" :key="project.id" class="my-10 md:flex gap-5">
+            <img :src="'/' + project.imageDirectory + '/' + project.images[0]" :alt="project.title"
+                class="block h-[250px] md:h-[200px] object-cover w-full rounded-md flex-2 order-1">
 
-            <p class="my-5">{{ project.description }}</p>
-
-            <router-link :to="`/portfolio/${project.id}`" class="link">Read more...</router-link>
+            <div class="flex-5">
+                <h3 class="font-black text-lg md:text-xl lg:text-2xl max-md:mt-5 mb-5">{{ project.title }}</h3>
+    
+                <p class="my-5">{{ project.description }}</p>
+    
+                <router-link :to="`/portfolio/${project.id}`" class="link">Read more...</router-link>
+            </div>
         </div>
     </Wrapper>
 </template>
